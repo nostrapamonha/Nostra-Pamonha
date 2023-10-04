d70e156fd0eeaf0e0e6ffb2e77640669e25f95ba
Iasmin Corrêa Rebelo <iasmin_correa@hotmail.com>
2023-10-04 13:42:28 -0300

    tags

--- Assets/js/stores.js
+++ Assets/js/stores.js
@@ -1,11 +1,17 @@
-$(function () {
-  $('.stores-carousel').slick({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-  });
+$('.stores-carousel').slick({
+  dots: false,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
