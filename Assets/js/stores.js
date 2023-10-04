d2c757296164668abfae688e6f0c7cc1905913db
Iasmin Corrêa Rebelo <iasmin_correa@hotmail.com>
2023-10-03 21:28:05 -0300

    lp

---
+++ Assets/js/stores.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.stores-carousel').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+  });
+});
